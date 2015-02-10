4ae51411867080c320dc0670622a278d4dc33139
Tom Fuertes <tomfuertes@gmail.com>
2015-02-10 13:53:17 -0600

    verbose push

--- bin/release.sh
+++ bin/release.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 rsync -v ./public/index.html clearhead@do.clrhd.me:~/index.html
-rsync --ignore-existing -raz ./public/assets/ clearhead@do.clrhd.me:~/assets/
+rsync --ignore-existing -razv ./public/assets/ clearhead@do.clrhd.me:~/assets/
