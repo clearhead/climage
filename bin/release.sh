a9f36a13d047ed2c65222f3701915ebfed4a7651
Tom Fuertes <TomFuertes@gmail.com>
2015-02-13 14:49:10 -0600

    Update release.sh

--- bin/release.sh
+++ bin/release.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 rsync -v ./public/index.html clearhead@do.clrhd.me:~/index.html
+rsync -v ./public/splash.html clearhead@do.clrhd.me:~/splash.html
 rsync --ignore-existing -razv ./public/assets/ clearhead@do.clrhd.me:~/assets/
